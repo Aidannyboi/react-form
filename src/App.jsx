9282069489c1b62a1512f9e21ca9cd533a010869
Aidan Castellanos <castellanosaidan@gmail.com>
2024-01-25 18:11:58 -0800

    Created a form that submits data and resets input

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import UserForm from './components/userForm'
+
+function App() {
+  return (
+    <div className='app'>
+      <UserForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/userForm.jsx
@@ -0,0 +1,71 @@
+import {useState} from "react";
+const UserForm = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [conPass, setConPass] = useState('');
+    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+
+
+const createUser = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        pass: pass,
+        conPass: conPass
+    };
+
+    setConPass("")
+    setPass("")
+    setEmail("")
+    setPass("")
+    setFirstName("")
+    setLastName("")
+    console.log("Created", newUser)
+
+    setHasBeenSubmitted(true);
+}
+
+return (
+    <form onSubmit={createUser}>
+        {
+            hasBeenSubmitted?
+            <h2>Thank you for submitting the form!</h2> :
+            <h2> Welcome, please submit the form.</h2>
+            }
+        <div className="form">
+            <div className="inputField">
+                <label> First Name: </label>
+                <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value)} className="userInput"/>
+            </div>
+
+            <div className="inputField">
+                <label> Last Name: </label>
+                <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value)} className="userInput"/>
+            </div>
+
+            <div className="inputField">
+                <label> Email: </label>
+                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value)} className="userInput"/>
+            </div>
+
+            <div className="inputField">
+                <label> Password: </label>
+                <input type="text" value={pass} onChange={ (e) => setPass(e.target.value)} className="userInput"/>
+            </div>
+            <div className="inputField">
+                <label> Confirm Password: </label>
+                <input type="text" value={conPass} onChange={ (e) => setConPass(e.target.value)} className="userInput"/>
+            </div>
+
+            <input type="submit" value="Create User" className="userSubmit"/>
+        </div>
+    </form>
+)
+}
+
+export default UserForm;
